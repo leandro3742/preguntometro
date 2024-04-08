fc4a255e6235a3ced40dd5bcfe76304467d28501
Leandro Marrero <leandro.marrero03@gmail.com>
2024-04-08 14:05:43 -0300

    elimino nav de chufles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -36,14 +36,14 @@ const Header = () => {
             </svg>
           </button>
           <div className={`w-full md:block md:w-auto ${isNavbarOpen ? '' : 'hidden'}`} id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {/* <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li onClick={toggleNavbar}>
                 <a href="/" className={classNameGenerator('/')}>Inicio</a>
               </li>
               <li onClick={toggleNavbar}>
                 <a href="/examen" className={classNameGenerator('/examen')}>Exámen</a>
               </li>
-            </ul>
+            </ul> */}
           </div>
         </>
       </div>
